f5d22002f8ad19c875dbf4e98ac1eec9f9dd2dab
James Shuttleworth <digehode@googlemail.com>
2022-03-07 19:50:38 +0000

    Added demo of printing, loops and selection

--- demo.py
+++ demo.py
@@ -1,1 +1,24 @@
-print("This is a demo of some python")
+name="James"
+age=18
+location="UK"
+
+
+for age in range(23):
+
+    if location=="UK":
+        oldEnough = age >= 18 
+    elif location=="USA":
+        oldEnough = age >= 21	
+    elif location=="SPAIN":
+        oldEnough = age >=19
+    else:
+        oldEnough = False
+        print("Assuming you aren't old enough because I don't know where you are")
+
+
+    if oldEnough:
+        print(f"Welcome {name} to the club! You are {age} years old")
+    else:
+        print(f"I am sorry, at {age} you are not old enough.")
+
+
